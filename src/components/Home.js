0f29be1b853c9f9d9302eaaea7762bd365bc6c0d
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-02-15 12:27:02 -0300

    android files for icon and splashscreen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,43 @@
 import React, {Component} from 'react';
-import { WebView } from 'react-native'
+import { WebView, View, ActivityIndicator, Dimensions } from 'react-native'
 
 export default class HomeScreen extends Component {
-  render() {
-    return <WebView source={{ uri: 'http://pequenocamaleao.com.br'}} />
+  constructor(props){
+    super(props)
+    this.state = {
+      loading: false
+    }
   }
-}
+  
+  render() {
+     return (
+       <View style={{ flex: 1 }}>
+          <WebView
+            onLoadStart={() => this.setState({ loading: true})}
+            onLoad={() => this.setState({ loading: false })}
+            source={{ uri: 'http://pequenocamaleao.com.br'}}
+          />
+          {this.state.loading && (
+            <ActivityIndicator
+              color="#ff7f00"
+              style={styles.loading}
+              size="large"
+            />
+          )}
+       </View>
+      )
+    }
+  } 
+
+  const styles = {
+    loading: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  
+  }
